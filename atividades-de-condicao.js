5ae9ec967d05fe58da0856a19497597f7ffb4300
vitoriadellaguardia <ancylavitoria@gmail.com>
2025-06-20 15:43:22 -0300

    atividades-de-condicao

---
+++ atividades-de-condicao.js
@@ -0,0 +1,18 @@
+// // questão 1 
+// let idade = 18;
+// if (idade >= 18) {
+//     console.log("Você é maior de idade.");
+// }else {
+//     console.log("Você é menor de idade.");
+// } 
+
+// questão 2
+let numero = 2;
+if (numero > 0) {
+    console.log(`${numero} é positivo.`);
+}else if (numero < 0) {
+        console.log(`${numero} é negativo.`);
+}else{
+    console.log(`é zero.`);
+}
+
---
+++ operadores.js
@@ -0,0 +1,45 @@
+// //operadores aritméticos 
+// console.log("2 + 2 = ", 2 + 2);
+// console.log("3 - 2 = ", 3 - 2);
+// console.log("2 * 6 = ", 2 * 6);
+// console.log("10 / 5 = ", 10 / 5);
+// console.log("10 / 5 = ", 10 % 5);
+
+//operadores atribuidores
+let numero =0;
+console.log("numero:", numero);
+numero = 2;
+console.log("atribui:", numero);
+// numero = numero + 3;
+numero += 3; 
+console.log("somei 3:", numero);
+numero -= 2;
+console.log("subtrai 2:", numero);
+numero *= 4;
+console.log("multipliquei por 4:", numero);
+numero /= 2;
+console.log("dividi por 2:", numero);
+
+// // operadores condicionais
+// console.log("2 > 1:", 2 > 1); // maior que
+// console.log("2 < 1:", 2 < 1); // menor que
+// console.log("2 >= 2:", 2 >= 2);
+// console.log("4 <= 3:", 4 <= 3);
+// console.log("1 == 2:", 1 == 1);
+// console.log("Vitoria == vitoria: ", 'Vitoria' == 'vitoria');
+// let senha = 1234;
+// console.log("senha == 1234:", senha == 1234);
+// console.log("senha != 1234:", senha != 12345); // diferente de
+// console.log("2 == 2: ", '2' == 2); 
+// console.log("2 != 2: ", '2' != 2);
+// console.log("2 === 2: ", '2' === 2); // estritamente igual
+// console.log("2 !== 2: ", '2' !== 2); // estritamente diferente
+
+
+// operadores lógicos
+console.log("2 && true:", 2 && true); // E lógico
+console.log("2 == 2 && 3 == 3:", 2 == 2 && 3 == 3); 
+console.log("2 == 3 || 3 == 3:", 2 == 3 || 3 == 3); 
+console.log("2 == 3 || 3 == 3:", 2 == 3 || 3 == 3); 
+
+
